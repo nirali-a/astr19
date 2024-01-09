5ba0e5e8a801c1746855f0846e79251a3a70a970
nirali-a <156133519+nirali-a@users.noreply.github.com>
2024-01-09 12:57:15 -0800

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,14 @@
+#This is a comment
+#This program will write Hello World!
+
+#This function tells the computer to print Hello World!
+def PrintHelloWorld():
+	print("Hello World!")
+
+#This is the program that will be run by python
+def main():
+	PrintHelloWorld()
+
+#Execute the main program when script is run
+if__name__=="__main__"
+	main()
