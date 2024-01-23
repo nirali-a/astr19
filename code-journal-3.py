48cdd4a4ffa94da57651716ece66c2bb4551c185
nirali-a <156133519+nirali-a@users.noreply.github.com>
2024-01-22 18:56:15 -0800

    Add files via upload

--- code-journal-3.py
+++ code-journal-3.py
@@ -3,10 +3,14 @@ import numpy as np
 def f(x):
 	return x**3 + 8
 
-x = 9
-
-print(f(x))
 
 
-if f(x)>27:
-	print("YAY")
+def main():
+	x = 9
+	print(f(x))
+
+
+	if f(x)>27:
+		print("YAY")
+
+main()
